8dc13527a1399d8b647e50ee9c575c990cd30761
ddarahakit2024 <ddarahakit2024@gmail.com>
2025-03-18 10:31:47 +0900

    bbb

--- src/main/java/com/example/jenkins_test/TestController.java
+++ src/main/java/com/example/jenkins_test/TestController.java
@@ -1,9 +1,14 @@
 package com.example.jenkins_test;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/test")
 @RestController
 public class TestController {
+    @GetMapping("/ex01")
+    public void ex01() {
+
+    }
 }
